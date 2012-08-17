10e05f723ada8cfc1a5ff9b0a75a9744c186e8f5
Sergio Lopez <wapankh@gmail.com>
2012-08-16 23:45:59 -0500

    Cleaning up the extra files created by the scaffold

--- app/controllers/appraisals_controller.rb
+++ app/controllers/appraisals_controller.rb
@@ -1,34 +1,49 @@
 class AppraisalsController < ApplicationController
-  # GET /appraisals
-  # GET /appraisals.json
-  def index
-    @appraisals = Appraisal.all
+  load_and_authorize_resource
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @appraisals }
+  # GET /appraisals
+  # GET /appraisals.xml
+  def index
+    @user = User.find_by_id(current_user)
+  
+    if @user.role.eql?("user")
+      redirect_to users_url
+    else
+      @appraisals = Appraisal.where(:assigned_to => current_user, :status =>EActivityValueClaimed )
+      @appraisals = Appraisal.where(:status => EActivityValueCreated) unless @appraisals.count > 0
+
+      respond_to do |format|
+        format.html # index.html.haml
+        format.xml  { render :xml => @appraisals }
+      end
     end
   end
 
   # GET /appraisals/1
-  # GET /appraisals/1.json
+  # GET /appraisals/1.xml
   def show
     @appraisal = Appraisal.find(params[:id])
+    @user = User.find_by_id(current_user)
+    if (!@appraisal.assigned_to.nil?)
+      @appraiser = User.find_by_id(@appraisal.assigned_to)
+    end
+    flash[:title] = "Appraisal" unless !flash[:title].nil?
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @appraisal }
+      format.html #{ render :layout => 'shareable' }# show.html.erb
+      format.xml  { render :xml => @appraisal }
     end
   end
 
   # GET /appraisals/new
-  # GET /appraisals/new.json
+  # GET /appraisals/new.xml
   def new
     @appraisal = Appraisal.new
+    1.times { @appraisal.photos.build }
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @appraisal }
+      format.html # new.html.haml
+      format.xml  { render :xml => @appraisal }
     end
   end
 
@@ -37,47 +52,89 @@ class AppraisalsController < ApplicationController
     @appraisal = Appraisal.find(params[:id])
   end
 
+  def reply
+    @appraisal = Appraisal.find(params[:id])
+    1.times { @appraisal.appraisal_datums.build }
+  end
+
   # POST /appraisals
-  # POST /appraisals.json
+  # POST /appraisals.xml
   def create
     @appraisal = Appraisal.new(params[:appraisal])
-
-    respond_to do |format|
+    @appraisal.created_by = current_user.id
+    @appraisal.status = EActivityValueCreated
+    
       if @appraisal.save
-        format.html { redirect_to @appraisal, notice: 'Appraisal was successfully created.' }
-        format.json { render json: @appraisal, status: :created, location: @appraisal }
+        session[:new_appraisal] = @appraisal
+        log_activity(@appraisal)
+        redirect_to payments_path
+        
       else
-        format.html { render action: "new" }
-        format.json { render json: @appraisal.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          flash[:error] = 'Appraisal cannot be created!'
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @appraisal.errors, :status => :unprocessable_entity }
+        end
       end
-    end
+    
   end
 
   # PUT /appraisals/1
-  # PUT /appraisals/1.json
+  # PUT /appraisals/1.xml
   def update
     @appraisal = Appraisal.find(params[:id])
 
     respond_to do |format|
       if @appraisal.update_attributes(params[:appraisal])
-        format.html { redirect_to @appraisal, notice: 'Appraisal was successfully updated.' }
-        format.json { head :no_content }
+        if @appraisal.status == EActivityValueFinalized
+          # Send Notification via Email to Creator about Finalized Appraisal
+          @appraisal.owned_by.notify_creator_of_appraisal_update( @appraisal )
+        end
+      log_activity(@appraisal)
+        format.html { redirect_to(@appraisal, :notice => 'Appraisal was successfully updated.') }
+        format.xml  { head :ok }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @appraisal.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @appraisal.errors, :status => :unprocessable_entity }
       end
     end
   end
 
+
   # DELETE /appraisals/1
-  # DELETE /appraisals/1.json
+  # DELETE /appraisals/1.xml
   def destroy
     @appraisal = Appraisal.find(params[:id])
     @appraisal.destroy
 
     respond_to do |format|
-      format.html { redirect_to appraisals_url }
-      format.json { head :no_content }
+      format.html { redirect_to(@appraisal, :notice => 'Appraisal was deleted successfully.') }
+      format.xml  { head :ok }
     end
   end
+  
+  def claim
+    @appraisal = Appraisal.find(params[:id])
+    if @appraisal.status == EActivityValueCreated
+      @appraisal.assigned_to = current_user
+      @appraisal.assigned_on = Time.now
+      @appraisal.status = EActivityValueClaimed
+      log_activity(@appraisal) unless !@appraisal.save
+      redirect_to reply_appraisal_path(@appraisal)
+    else
+      redirect_to(@appraisal, :alert => "You cannot claim this appraisal at this time")
+    end
+  end
+
+  protected
+
+  def log_activity(appraisal)
+    activity = AppraisalActivity.new({:appraisal_id   => appraisal.id,
+                                      :user_id      => current_user.id,
+                                      :activity_type  => User.roles.index(current_user.role),
+                                      :activity_value => appraisal.status
+                                     })
+    activity.save
+  end
+    
 end
