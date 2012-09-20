6181cb272069798aa06f7fe722f0e03d995f5886
Sergio Lopez <wapankh@gmail.com>
2012-09-19 19:18:00 -0500

    Misc fixes

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PurexNew::Application.initialize!
-PAYMENT_PLAN = Array.[](19.00, 29.00) #define in appraisals.selected_plan (0=>4.99, 1=>9.99)
+
+# TODO Move this to a separate config file
+PAYMENT_PLAN = Array.[](19.00, 29.00) #define in appraisals.selected_plan (0=>19.00, 1=>29.00)
