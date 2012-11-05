bd6c865b337fe91d3eb2b5dc2bad25e4fa263bd7
Sergio Lopez <wapankh@never-forget.lan>
2012-11-04 18:26:30 -0600

    #120: Appraiser Contact: Mark required fields, #121: Appraiser Contact Tab: Add email field

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ActiveRecord::Base
   belongs_to :user
   attr_accessible :address, :address2, :city, :country, :fax, :phone1, :phone2, :phone3, :state, :zip
+
+  validates_presence_of :address, :city, :country, :phone1, :state, :zip, :unless => :address_optional
+
+  private
+  def address_optional
+  	self.user.class.to_s.eql?("Customer")
+  end
 end
