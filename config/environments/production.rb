2484cc835ee6f633090f07ff145a68aceaa08795
Sergio Lopez <wapankh@never-forget.lan>
2012-12-06 12:50:53 -0600

    #40779061 Link in confirmation email should use beta.valuethisnow.com instead of dry-tundra

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ PurexNew::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => ENV['HOST_NAME'] }
+  config.action_mailer.default_url_options = { :host => 'beta.valuethisnow.com' }
   config.action_mailer.raise_delivery_errors = true
 
   ActionMailer::Base.smtp_settings = {
