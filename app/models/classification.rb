509928ffa9dd9c3b38fd89557ca0bf0aeafa7d07
Sergio Lopez <wapankh@tardis.lan>
2012-12-16 18:54:23 -0600

    #41186917 Classification must have a Category_id

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,4 +2,6 @@ class Classification < ActiveRecord::Base
   belongs_to :appraisal
   belongs_to :category
   attr_accessible :appraisal_id, :category_id
+
+  validates_presence_of :appraisal_id, :category_id
 end
