406963754568117bf542fe8e1e0bba28735fdc3e
Sergio Lopez <wapankh@never-forget.lan>
2013-01-15 10:37:07 -0600

    #42570695 Appraiser cannot add the same category twice

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ActiveRecord::Base
   belongs_to :appraiser
   belongs_to :category
   attr_accessible :discipline, :since, :user_id, :category_id, :appraiser_id
-  # validate :check_for_duplicate
+  validate :check_for_duplicate
 
 	# For backward compatibility
 	def user_id
@@ -14,8 +14,8 @@ class Skill < ActiveRecord::Base
 	end
 
   def check_for_duplicate
-    unless self == self.appraiser.skills.find_by_discipline(self.discipline)
-      self.errors.add( :base, "Already have that discipline") if self.appraiser.skills.find_by_discipline(self.discipline)
+    unless self == self.appraiser.skills.find_by_category_id(self.category_id)
+      self.errors.add( :base, "Already have that discipline") if self.appraiser.skills.find_by_category_id(self.category_id)
     end
   end
 
