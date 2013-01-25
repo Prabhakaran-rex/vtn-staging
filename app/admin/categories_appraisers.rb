ca8bdf1c80c644cb6f6613ce4f25ab37a4a249d4
Sergio Lopez <wapankh@never-forget.lan>
2013-01-24 21:31:11 -0600

    #43103559 Add Category vs Appraisers and Appraisers vs Category

---
+++ app/admin/categories_appraisers.rb
@@ -0,0 +1,20 @@
+ ActiveAdmin.register_page "Categories vs Appraisers" do
+   menu :label => "Categories vs Appraisers", :parent => "Reports"
+    content do
+      render :partial => "admin/reports/categories_appraisers"
+    end
+ end
+
+ ActiveAdmin.register_page "Appraisers vs Categories" do
+   menu :label => "Appraisers vs Categories", :parent => "Reports"
+    content do
+      render :partial => "admin/reports/appraisers_categories"
+    end
+ end
+
+ ActiveAdmin.register_page "Orphan Categories" do
+   menu :label => "Orphan Categories", :parent => "Reports"
+    content do
+      render :partial => "admin/reports/orphan_categories"
+    end
+ end
