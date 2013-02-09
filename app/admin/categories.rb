6ffb7db0fdd541a0f4711bd2a259773b8ff4e090
Sergio Lopez <wapankh@never-forget.lan>
2013-02-09 14:06:14 -0600

    #44148223 Admin can create categories

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Category do
   actions :index, :show, :new, :create, :update, :edit
+
+  form do |f|
+    f.inputs "Category Details" do
+      f.input :name
+    end
+    f.buttons
+  end
+
 end
