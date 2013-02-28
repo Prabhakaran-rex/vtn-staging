dfdbccfc674067872299f03d2d0d9905e99a01d1
Sergio Lopez <wapankh@never-forget.lan>
2013-02-28 10:12:39 -0600

    Refactoring: Sign in/sign up tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,16 @@
 FactoryGirl.define do
-  
+  sequence(:email){|n| "example#{n}@example.net" }
+  sequence(:username) {|n| "test_user_#{n}" }
+
+  factory :user do
+    email
+    role 'customer'
+    name 'Test User'
+    username
+    status EAUserStatusConfirmed
+    password 'Plea$e1234'
+    password_confirmation 'Plea$e1234'
+    confirmed_at Time.now
+    type 'Customer'
+  end
 end
