c1904d86c2be7a8341d45e9c5e64a54bcb2b03cd
Sergio Lopez <wapankh@never-forget.lan>
2013-05-20 17:09:05 -0500

    #49266732: Coupons and Promotions (Fields in Admin)

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -1,6 +1,22 @@
 ActiveAdmin.register Coupon do
 	actions :all, :except => [:destroy]
 
+  index do
+    selectable_column
+    column :id
+    column :code
+    column :discount
+    column :discount_type
+    column :active
+    column :featured
+    column :start_date
+    column :expiration_date
+    column :used_on
+    column :max_usage
+    column :description
+    default_actions
+  end
+
   form do |f|
     f.inputs "Coupon Details" do
       f.input :promotion
@@ -9,7 +25,7 @@ ActiveAdmin.register Coupon do
       f.input :max_usage, hint: "The maximum number of times the same coupon can be used"
       f.input :discount, hint: "For percentage discounts (20%) enter '20'; for fixed discounts enter the amount in dollars"
       f.input :discount_type, :as => :select, :collection => ["fixed", "percentage"]
-      f.input :max_discount, hint: "If entered, this is the maximum amount that can be deducted from the price"
+      f.input :max_discount, hint: "If entered, this is the maximum amount that will be deducted from the price"
       f.input :start_date
       f.input :expiration_date
       f.input :description, hint: "Will be shown in places like the featured box in the home page"
