7e7150c75ab83b5ea2b56db1729cbe64d755c50e
Sergio Lopez <wapankh@tardis.lan>
2013-08-21 10:45:44 -0500

    #55562538: User redirection to appraisal after sing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource_or_scope)
+    return session[:user_return_to] if session[:user_return_to]
     if (current_user.admin?)
       admin_dashboard_path
     elsif(current_user.is_appraiser?)
@@ -19,7 +20,6 @@ class ApplicationController < ActionController::Base
     else
       users_path
     end
-
   end
 
   def refinery_user_required?
@@ -33,10 +33,20 @@ class ApplicationController < ActionController::Base
       redirect_to root_path 
     end
   end
-  
+
   def current_admin_user
     return nil if user_signed_in? && !current_user.admin?
     current_user
+  end
+
+  def get_index_for_user_type(user)
+    if (user.admin?)
+      admin_dashboard_path
+    elsif(user.is_appraiser?)
+      dashboard_detail_path
+    else
+      users_path
+    end
   end
 
   protected
