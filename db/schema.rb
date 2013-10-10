4d4c25746cadc229be7b9728f03ba1e17814c6d0
Sergio Lopez <wapankh@tardis.lan>
2013-10-10 18:54:19 -0500

    #58677908: Appraisal visibility switch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131007235411) do
+ActiveRecord::Schema.define(:version => 20131010232410) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -439,6 +439,14 @@ ActiveRecord::Schema.define(:version => 20131007235411) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "settings", :force => true do |t|
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
+  end
 
   create_table "skills", :force => true do |t|
     t.string   "discipline"
